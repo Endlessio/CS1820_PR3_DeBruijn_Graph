92d36ca2493925563d92e1477726f203fc55ec30
Endlessio <117010423@link.cuhk.edu.cn>
2022-03-24 22:19:31 -0400

    part1

--- src/assembly.py
+++ src/assembly.py
@@ -1,12 +1,12 @@
 import contamination
 import correction
-import preprocessing
+from preprocessing import get_reads, get_score_matrix, get_vector
 import sys
 
 '''
 global variable
 '''
-ROOT_DIR = "../test_cases/assembly"
+ROOT_DIR = "../test_cases/assembly/"
 
 
 class Assembly:
@@ -16,6 +16,13 @@ class Assembly:
 
 def main(argv):
     # preprocess data
+    vector = get_vector(ROOT_DIR+argv[1])
+    reads = get_reads(ROOT_DIR+argv[2])
+    contamination_k = int(argv[3])
+    correction_k = int(argv[4])
+    correction_d = int(argv[5])
+    correction_t = int(argv[6])
+    correction_mode = argv[7]
 
     # contamination to delete the reads
 
